8831b6496835c3ee4ab054b78cd260be852bb9d3
Asteria101 <lebrito1268@gmail.com>
2024-04-08 21:11:41 -0300

    Add final version of lab 2

--- lab02/main.c
+++ lab02/main.c
@@ -3,13 +3,31 @@
 #include <stdarg.h>
 
 
+/*
+* Raise error
+*/
+void Error(char *msg, char *function, ...) {
+    va_list args; /* list of arguments */
+    char msg_final[4096];
+
+    va_start(args, function);
+    vsprintf(msg_final, msg, args);
+    va_end(args);
+
+    printf("Erro em %s: %s\n", function, msg_final);
+    fflush(stdout);
+    exit(1);
+}
+
+
 #ifndef bool
 typedef enum Bool {false, true} bool;
 #endif
 
 typedef struct Point {
-    int x, y, z;
+    int x, y, z;    
 }point;
+
 
 typedef struct Maze {
     char ***data;
@@ -21,20 +39,6 @@ typedef struct Maze {
 typedef maze *p_maze;
 
 
-void Erro(char *msg, char *funcao, ...) {
-    va_list args; /* lista de argumentos */
-    char msg_final[4096];
-
-    va_start(args, funcao); /* inicializa a lista args para recuperar argumentos após o argumento funcao */
-    vsprintf(msg_final, msg, args); /* envia a lista inteira de argumentos em args para formatação em msg_final */
-    va_end(args); /* finaliza o processo de recuperação */
-
-    printf("Erro em %s: %s\n",funcao,msg_final);
-    fflush(stdout);
-    exit(1);
-}
-
-
 /* 
 * Reads the information to build the maze from a file
 */
@@ -42,8 +46,6 @@ p_maze read_info(char *filename) {
     FILE *fp = fopen(filename, "r");
 
     p_maze M = (p_maze)calloc(1, sizeof(maze));
-
-    //scanf("%d %d %d %d\n", &M->nx, &M->ny, &M->nz, &M->n_entries);
 
     fscanf(fp, "%d %d %d %d\n", &M->nx, &M->ny, &M->nz, &M->n_entries);
 
@@ -53,19 +55,16 @@ p_maze read_info(char *filename) {
     // allocating memory for the maze
     M->data = (char ***)calloc(M->nz, sizeof(char **));
 
-    for (int z = 0; z < M->nz; z++) {
-        M->data[z] = (char **)calloc(M->ny, sizeof(char *));
-        for (int y = 0; y < M->ny; y++) {
-            M->data[z][y] = (char *)calloc(M->nx, sizeof(char));
-        }
-    }
-
-    // writing on the maze
     int j = 0, i = 0;
     for (int z = 0; z < M->nz; z++) {
+        M->data[z] = (char **)calloc(M->ny, sizeof(char *));
+
         for (int y = 0; y < M->ny; y++) {
+            M->data[z][y] = (char *)calloc(M->nx, sizeof(char));
+
             for (int x = 0; x < M->nx; x++) {
-                fscanf(fp, "%c   ", &M->data[z][y][x]);
+                fscanf(fp, "%c", &M->data[z][y][x]);
+                fscanf(fp, "   ");
 
                 if (M->data[z][y][x] == 'E') {
                     M->entry[i].z = z, M->entry[i].y = y, M->entry[i].x = x;
@@ -82,6 +81,7 @@ p_maze read_info(char *filename) {
                 }
             }
             fscanf(fp, "\n");
+
         }
     }
 
@@ -91,7 +91,7 @@ p_maze read_info(char *filename) {
 
 
 /*
-* Frees the allocated memory for the maze
+* Frees allocated memory for the maze
 */
 void destroy_maze(p_maze M) {
     if (M == NULL) exit(1);
@@ -120,52 +120,33 @@ void destroy_maze(p_maze M) {
 bool backtrack(p_maze M, point P) {
     bool ans = false;
     
+    // checks if point is an exit
     for (int j = 0; j < M->n_exits; j++) {
         if ((P.z == M->exit[j].z) && (P.y == M->exit[j].y) && (P.x == M->exit[j].x)) {
-            return true;
-        }
-        else {
-            point *C = (point *)calloc(6, sizeof(point));
-
-            C[0].z = P.z;
-            C[0].y = P.y;
-            C[0].x = P.x - 1;
-
-            C[1].z = P.z;
-            C[1].y = P.y;
-            C[1].x = P.x + 1;
-
-            C[2].z = P.z;
-            C[2].y = P.y - 1;
-            C[2].x = P.x;
-
-            C[3].z = P.z;
-            C[3].y = P.y + 1;
-            C[3].x = P.x;
-
-            C[4].z = P.z - 1;
-            C[4].y = P.y;
-            C[4].x = P.x;
-
-            C[5].z = P.z + 1;
-            C[5].y = P.y;
-            C[5].x = P.x;
-
-            for (int i = 0; (i < 6) && (ans != true); i++) {
-                if ((C[i].z >= 0) && (C[i].z < M->nz) && (C[i].y >= 0) && (C[i].y < M->ny) && (C[i].x >= 0) && (C[i].x < M->nx)) {
-                    if (M->data[C[i].z][C[i].y][C[i].x] != 'X') {
-                        char correct_value = M->data[C[i].z][C[i].y][C[i].x];
-                        M->data[C[i].z][C[i].y][C[i].x] = 'X';
-
-                        ans = backtrack(M, C[i]);
-                        M->data[C[i].z][C[i].y][C[i].x] = correct_value;
-                    }
-                }
-            }
-            free(C);
+            return ans = true;
         }
     }
     
+    point neighbors[6] = {
+        {P.x - 1, P.y, P.z}, 
+        {P.x + 1, P.y, P.z}, 
+        {P.x, P.y - 1, P.z}, 
+        {P.x, P.y + 1, P.z},
+        {P.x, P.y, P.z - 1},
+        {P.x, P.y, P.z + 1}
+    };
+
+    for (int i = 0; (i < 6) && (ans != true); i++) {
+        if ((neighbors[i].z >= 0) && (neighbors[i].z < M->nz) && (neighbors[i].y >= 0) && (neighbors[i].y < M->ny) && (neighbors[i].x >= 0) && (neighbors[i].x < M->nx)) {
+            if (M->data[neighbors[i].z][neighbors[i].y][neighbors[i].x] != 'X') {
+                char correct_value = M->data[neighbors[i].z][neighbors[i].y][neighbors[i].x];
+                M->data[neighbors[i].z][neighbors[i].y][neighbors[i].x] = 'X';
+                ans = backtrack(M, neighbors[i]);
+                M->data[neighbors[i].z][neighbors[i].y][neighbors[i].x] = correct_value;
+            }
+        }
+    }
+
     return ans;
 }
 
@@ -174,21 +155,10 @@ int main(int argc, char **argv) {
     p_maze Maze = NULL;
 
     if (argc != 2) {
-        Erro("%s <arquivo texto do labirinto>", "main", argv[0]); 
+        Error("%s <arquivo texto do labirinto>", "main", argv[0]); 
     }
 
     Maze = read_info(argv[1]);
-    //Maze = read_info();
-
-    /*for (int i = 0; i < Maze->nz; i++) {
-        for (int j = 0; j < Maze->ny; j++) {
-            for (int k = 0; k < Maze->nx; k++) {
-                printf("%c   ", Maze->data[i][j][k]);
-            }
-            printf("\n");
-        }
-        printf("\n");
-    }*/
 
     for (int i = 0; i < Maze->n_entries; i++) {
         if (backtrack(Maze, Maze->entry[i])) {
